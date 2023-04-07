b2fe2f2cedb10d31c0c1667f552fdac1e0884c58
uwjiaoxing <uwjiaoxing@gmail.com>
2023-04-07 22:37:08 +0800

    chore: support eslint@^8 and typescript@^4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
   extends: [
     'eslint-config-ali/typescript/vue',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/vue',
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
+    'no-unused-vars': 'warn',
     '@typescript-eslint/method-signature-style': 'warn',
     // 可以使用嵌套的三元表达式
     'no-nested-ternary': 'off',
     'import/no-cycle': 'warn',
   },
   parserOptions: {
-    // 提升解析速度
     parser: {
       js: '@typescript-eslint/parser',
       ts: '@typescript-eslint/parser',
       '<template>': 'espree',
     },
+    ecmaVersion: 2022,
   },
   overrides: [
     {
